f88f5b5ef4a0adc281e958e2a36208eda5b70905
Orchid <137605918+orchidcnr@users.noreply.github.com>
2023-06-25 19:29:51 -0400

    Update password-generator.py with pyperclips and error messages

--- python/password-generator.py
+++ python/password-generator.py
@@ -1,22 +1,34 @@
 import string
 import secrets
+import pyperclip
 
-# Question variables
-size = input("How many characters would you like your password to be? ")
-type = input("Would you like your password to be simple (1) or complex (2)? ")
-
-# Answer variables
+# Variables
 simple = string.ascii_letters + string.digits
 complex = simple + string.punctuation
 
+# Questions
+try:
+    size = int(input("How many characters would you like your password to be? "))
+except ValueError:
+    size = input("Please provide an answer in the form of an integer. (¬_¬ ) ")
+type = input("Would you like your password to be (s)imple or (c)omplex? ")
+
 # Password generator
 while True:
-    if type == "1":
+    if type == "s":
         password = ''.join(secrets.choice(simple) for i in range (int(size)))
         print(password)
-    if type == "2":
+        pyperclip.copy(password)
+    elif type == "c":
         password = ''.join(secrets.choice(complex) for i in range (int(size)))
         print(password)
+        pyperclip.copy(password)
+    else:
+        type = input('The answer you provided means nothing to my python script headass. Please provide an answer containing either "s" or "c". Also, you look great today! (≧◡≦)♡ ')
+        continue
+    # Regenerate password
     again = input("Generate again? (y/n) ")
-    if again != "y":
+    if again == "y":
+        True
+    else:
         break
