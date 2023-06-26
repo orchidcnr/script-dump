9e9a8f5b4ae01d51e5980ee1e6663cb51add01ec
Orchid <137605918+orchidcnr@users.noreply.github.com>
2023-06-25 20:11:44 -0400

    Update password-generator.py

--- python/password-generator.py
+++ python/password-generator.py
@@ -7,27 +7,31 @@ simple = string.ascii_letters + string.digits
 complex = simple + string.punctuation
 
 # Questions
-try:
-    size = int(input("How many characters would you like your password to be? "))
-except ValueError:
-    size = input("Please provide an answer in the form of an integer. (¬_¬ ) ")
+while True:
+    try:
+        size = int(input("How many characters would you like your password to be? "))
+    except ValueError:
+        print("Please provide an answer in the form of an integer. (¬_¬ ) ")
+        continue
+    else:
+        break
 type = input("Would you like your password to be (s)imple or (c)omplex? ")
 
 # Password generator
 while True:
     if type == "s":
-        password = ''.join(secrets.choice(simple) for i in range (int(size)))
+        password = ''.join(secrets.choice(simple) for i in range(size))
         print(password)
         pyperclip.copy(password)
     elif type == "c":
-        password = ''.join(secrets.choice(complex) for i in range (int(size)))
+        password = ''.join(secrets.choice(complex) for i in range(size))
         print(password)
         pyperclip.copy(password)
     else:
         type = input('The answer you provided means nothing to my python script headass. Please provide an answer containing either "s" or "c". Also, you look great today! (≧◡≦)♡ ')
         continue
     # Regenerate password
-    again = input("Generate again? (y/n) ")
+    again = input("Copied to clipboard! Generate again? (y/n) ")
     if again == "y":
         True
     else:
